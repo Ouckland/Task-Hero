6a3d221b0999061dfe56dcfd1d8d6c5ebcf87ff7
Isele Korede Michael B. <iselekorede5@gmail.com>
2025-05-17 00:39:18 +0100

    pushing to host the project on render

--- config/settings.py
+++ config/settings.py
@@ -20,12 +20,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0^uq_x*1uaqbfgu!3g@x636fiug39by*ty(+1n(kt-*j0f31lt'
+import os
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'fallback-secret-key-for-dev-only')
+
+# SECRET_KEY = 'django-insecure-0^uq_x*1uaqbfgu!3g@x636fiug39by*ty(+1n(kt-*j0f31lt'
+
+
+import os
+
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost:', '127.0.0.1']
 
 
 # Application definition
@@ -117,8 +126,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS = [BASE_DIR / 'static']
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
